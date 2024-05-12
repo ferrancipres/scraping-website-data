6fb1678f6e2dfa78ed9c230aaaa7b20d7f1fc45c
Ferran Cipres <ferrancipres@gmail.com>
2024-05-12 20:11:31 +0200

    server is running on port

---
+++ src/config/config.js
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+
+if(process.env.NODE_ENV === 'production'){
+    dotenv.config({ path: '.env.production' });
+} else {
+    dotenv.config({ path: '.env.development' });
+}
+
+//Definir entorno
+const ENV = process.env.NODE_ENV ?? 'development';
+
+// Definir configuración
+const CONFIG = {
+    development: {
+    app: {
+        PORT: process.env.PORT || 3001
+    },
+},
+    production : {
+        app: {
+            PORT: process.env.PORT || 3002
+        }
+    }
+};
+
+export default CONFIG[ENV];
