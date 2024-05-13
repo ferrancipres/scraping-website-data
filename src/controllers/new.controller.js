8308739da4398de594ee153b4c7293486f761aba
Ferran Cipres <ferrancipres@gmail.com>
2024-05-13 21:26:46 +0200

    [fix]: problem with controller dinamic fixed

--- src/controllers/new.controller.js
+++ src/controllers/new.controller.js
@@ -1,11 +1,6 @@
 import { scrapeNews } from '../scraper.js';
-// import { scrapeNewsByPage } from '../scraper.js';
+import { scrapeNewsByPage } from '../scraper.js';
 
-//controller getNews
-//scraper génerico de primera página
-// export const getNews = async (req, res) => {
-//     res.status(200).send('Get All News Ferran');
-// };
 export const getNews = async (req, res) => {
     try {
         const news = await scrapeNews();
@@ -15,20 +10,14 @@ export const getNews = async (req, res) => {
     }
 };
 
-//scraper con número de página
-// export const getNewsByPage = async (req, res) => {
-//     //pageId es el número de la página y es el query param
-//     const i = req.params.pageId;
-//     console.log(i);
-//     // res.status(200).send('Get News by Page');
-//     try {
-//         const news = await scrapeNewsByPage(i);
-//         res.status(200).json(news);
-//     } catch (error) {
-//         res.status(500).send(error.message);
-//     }
-//     //linea de código para sacar el número por consola
-//     // console.log(req.params.number);
-//     // res.status(200).send('Get News by Number Ferran');
-// };
+// scraper con número de página
+export const getNewsByPage = async (req, res) => {
+    const i = req.params.pageId;
+    try {
+        const news = await scrapeNewsByPage(i);
+        res.status(200).json(news);
+    } catch (error) {
+        res.status(500).send(error.message);
+    }
+};
 
--- src/routes/scraper.routes.js
+++ src/routes/scraper.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getNews } from "../controllers/new.controller.js";
+import { getNews, getNewsByPage } from "../controllers/new.controller.js";
 
 const scraperRoutes = Router();
 
@@ -30,53 +30,34 @@ const scraperRoutes = Router();
 
 scraperRoutes.get('/', getNews);
 
-// /**
-//  * @openapi
-//  * /pageId:
-//  *  get:
-//  *      tags:    
-//  *          - News
-//  *      parameters:
-//  *          - in: path
-//  *            name: pageId
-//  *            schema:
-//  *              type: integer
-//  *            required: true
-//  *            description: The id of the page to retrieve
-//  *      responses:
-//  *          200:
-//  *              description: Returns the news
-//  *              content:
-//  *                  application/json:
-//  *                      schema:
-//  *                          type: object
-//  *                          properties:
-//  *                              status:
-//  *                                  type: string
-//  *                                  example: OK
-//  *                              data:  
-//  *                                  type: array
-//  *                                  items:
-//  *                                      type: object
-//  *                                      properties: 
-//  *                                          // properties of the items in the data array go here
-//  *          500: 
-//  *              description: Internal Server Error
-//  *              content: 
-//  *                  application/json:
-//  *                      schema:
-//  *                          type: object
-//  *                          properties:
-//  *                              status:  
-//  *                                  type: string
-//  *                                  example: Internal Server Error
-//  *                              data: 
-//  *                                  type: object
-//  *                                  properties:
-//  *                                      error:
-//  *                                          type: string
-//  *                                          example: Error message
-//  */
-// scraperRoutes.get('/:pageId', getNewsByPage);
+/**
+ * @openapi
+ * /{pageId}:
+ *  get:
+ *      tags:    
+ *          - News
+ *      parameters:
+ *          - in: path
+ *            name: pageId
+ *            schema:
+ *              type: integer
+ *            required: true
+ *            description: The id of the page to retrieve
+ *      responses:
+ *          200:
+ *              description: Returns the news
+ *              content:
+ *                  application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              status:
+ *                                  type: string
+ *                                  example: OK
+ *                              data:  
+ *                                  type: array
+ */
+
+scraperRoutes.get('/:pageId', getNewsByPage);
 
 export default scraperRoutes;
