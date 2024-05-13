833d0091db94ac529d73ab93ca24b1edded950ba
Ferran Cipres <ferrancipres@gmail.com>
2024-05-13 12:00:07 +0200

    [Add]: Swagger library on both endpoints to return information with documentation

--- src/routes/scraper.routes.js
+++ src/routes/scraper.routes.js
@@ -3,8 +3,57 @@ import { getNews, getNewsByNumber } from "../controllers/new.controller.js";
 
 const scraperRoutes = Router();
 
-//Aquí debería de crear la ruta genérica
+/**
+ * @openapi
+ * /:
+ *  get:
+ *      tags:    
+ *          - News
+ *      responses:
+ *         200:
+ *          description: Returns the news
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          status:
+ *                              type: string
+ *                              example: OK
+ *                          data:  
+ *                              type: array
+ *                              items:
+ *                                 type: object
+ *                                 properties: 
+ *                                    // properties of the items in the data array go here
+ */
+
 scraperRoutes.get('/', getNews);
+
+/**
+ * @openapi
+ * /number:
+ *  get:
+ *      tags:    
+ *          - News
+ *      responses:
+ *         200:
+ *          description: Returns the news
+ *          content:
+ *              application/json:
+ *                  schema:
+ *                      type: object
+ *                      properties:
+ *                          status:
+ *                              type: string
+ *                              example: OK
+ *                          data:  
+ *                              type: array
+ *                              items:
+ *                                 type: object
+ *                                 properties: 
+ *                                    // properties of the items in the data array go here
+ */
 scraperRoutes.get('/:number', getNewsByNumber);
 
 export default scraperRoutes;
