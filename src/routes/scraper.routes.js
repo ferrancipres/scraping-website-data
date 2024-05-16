7f636f065110fe4a5bd80fc2baf2b7bc0660ed26
Ferran Cipres <ferrancipres@gmail.com>
2024-05-16 19:00:27 +0200

    [update]: clean branch, comments, change configuration controller

--- src/routes/scraper.routes.js
+++ src/routes/scraper.routes.js
@@ -3,7 +3,6 @@ import { getNews, getNewsByNumber } from "../controllers/new.controller.js";
 
 const scraperRoutes = Router();
 
-//Aquí debería de crear la ruta genérica
 scraperRoutes.get('/', getNews);
 scraperRoutes.get('/:number', getNewsByNumber);
 
