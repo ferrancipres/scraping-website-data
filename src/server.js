ab646385ac2bff14c8c5c9907aacdfafad1ce39e
Ferran Cipres <ferrancipres@gmail.com>
2024-05-23 19:30:19 +0200

    [add]: middleware apicache

--- src/server.js
+++ src/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
 import scraperRoutes from './routes/scraper.routes.js';
+import apicache from 'apicache';
 
 const app = express();
+const cache = apicache.middleware;
+
 app.use(express.json());
+app.use(cache('1 minutes'));
 app.use('/', scraperRoutes);
 
 export default app;
