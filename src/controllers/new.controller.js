e87cada9b5523d45d52666b5edf278f3cbc4d3dc
Ferran Cipres <ferrancipres@gmail.com>
2024-05-24 07:00:00 +0200

    [add]: comments

--- src/controllers/new.controller.js
+++ src/controllers/new.controller.js
@@ -36,52 +36,56 @@ createClient()
     }
   });
   
-export const getNews = async (req, res) => {
-  try {
-    if (redisAvailable) {
-      const reply = await client.get('news_1');
-      if(reply) {
-        return res.status(200).json(JSON.parse(reply));
-      }
-    }
-
-    const news = await scrapeNews();
-    if (redisAvailable) {
-      await client.set('news_1', JSON.stringify(news));
-    }
-    res.status(200).json(news);
-  } catch(err) {
-    console.error(err);
-  }
-};
-
-export const getNewsByPage = async (req, res) => {
-  const pageId = req.params.pageId;
-  try {
-    let lastPageInCache = 0;
-    let news = [];
-    if (redisAvailable) {
-      for (let i = 1; i <= pageId; i++) {
-        const reply = await client.get(`news_${i}`);
+  export const getNews = async (req, res) => {
+    try {
+      if (redisAvailable) {
+        const reply = await client.get('news_1');
         if(reply) {
-          news.push(...JSON.parse(reply));
-          lastPageInCache = i;
+          console.log('Retrieved news from Redis');
+          return res.status(200).json(JSON.parse(reply));
         }
       }
+  
+      const news = await scrapeNews();
+      if (redisAvailable) {
+        await client.set('news_1', JSON.stringify(news));
+      }
+      console.log('Retrieved news by scraping');
+      res.status(200).json(news);
+    } catch(err) {
+      console.error(err);
     }
-
-    if (!redisAvailable || lastPageInCache < pageId) {
-      for (let i = lastPageInCache + 1; i <= pageId; i++) {
-        const pageNews = i === 1 ? await scrapeNews() : await scrapeNewsByPage(i);
-        news.push(...pageNews);
-        if (redisAvailable) {
-          await client.set(`news_${i}`, JSON.stringify(pageNews));
+  };
+  
+  export const getNewsByPage = async (req, res) => {
+    const pageId = req.params.pageId;
+    try {
+      let lastPageInCache = 0;
+      let news = [];
+      if (redisAvailable) {
+        for (let i = 1; i <= pageId; i++) {
+          const reply = await client.get(`news_${i}`);
+          if(reply) {
+            console.log(`Retrieved news for page ${i} from Redis`);
+            news.push(...JSON.parse(reply));
+            lastPageInCache = i;
+          }
         }
       }
+  
+      if (!redisAvailable || lastPageInCache < pageId) {
+        for (let i = lastPageInCache + 1; i <= pageId; i++) {
+          const pageNews = i === 1 ? await scrapeNews() : await scrapeNewsByPage(i);
+          console.log(`Retrieved news for page ${i} by scraping`);
+          news.push(...pageNews);
+          if (redisAvailable) {
+            await client.set(`news_${i}`, JSON.stringify(pageNews));
+          }
+        }
+      }
+  
+      res.status(200).json(news);
+    } catch (error) {
+      res.status(500).send(error.message);
     }
-
-    res.status(200).json(news);
-  } catch (error) {
-    res.status(500).send(error.message);
-  }
-};
+  };
